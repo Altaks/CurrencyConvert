ae29da7ea99d4194d9cdd792586eb05fa7957cda
Altaks <57632793+Altaks@users.noreply.github.com>
2022-05-18 20:19:44 +0200

    Change API (more requests), missing the reading part of the api response

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// api key = cwyl15j9vpMuRrGWJcH4uZT83XJS6r9N
+// api key = f47ba4dffba7bea0f2c859a1971e10c3f0bc4255
 
 const firstInput = document.getElementById("first-input");
 const secondInput = document.getElementById("second-input");
@@ -8,25 +8,66 @@ const secondCurrencySelector = document.getElementById("second-currency");
 
 // query all the currencies that exists currently
 
-var headers = new Headers();
-headers.append('apikey', 'cwyl15j9vpMuRrGWJcH4uZT83XJS6r9N');
-
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
-    headers: headers
-}
-
-fetch("https://api.apilayer.com/fixer/symbols", requestOptions)
+fetch("https://api.getgeoapi.com/v2/currency/list" +
+    "?api_key=f47ba4dffba7bea0f2c859a1971e10c3f0bc4255" +
+    "&format=json")
     .then(response => response.json())
-    .then(result => {
-        for(let currency in result.symbols){
-            var opt = document.createElement('option');
-            opt.value = currency;
-            opt.innerHTML = currency;
-            firstCurrencySelector.appendChild(opt);
-            secondCurrencySelector.appendChild(opt.cloneNode(true));
+    .then(results => {
+        for(let elem in results.currencies){
+            let option = document.createElement("option");
+            option.value = elem;
+            option.innerText = elem;
+            firstCurrencySelector.appendChild(option);
+            secondCurrencySelector.appendChild(option.cloneNode(true));
         }
     })
-    .catch(error => console.log('error', error));
+    .then(data => console.log(data));
 
+// add auto convert from first currency to second currency, by checking the selected currency on the second input
+firstInput.addEventListener('input', function(event) {
+
+    let firstCurrency = firstCurrencySelector.value;
+    let secondCurrency = secondCurrencySelector.value;
+
+    let firstValue = firstInput.value;
+
+    if(firstValue === ""){
+        secondInput.value = "";
+    } else {
+        fetch("https://api.getgeoapi.com/v2/currency/convert" +
+            "?api_key=f47ba4dffba7bea0f2c859a1971e10c3f0bc4255" +
+            "&from=" + firstCurrency +
+            "&to=" + secondCurrency +
+            "&amount=" + firstValue +
+            "&format=json")
+            .then(response => response.json())
+            .then(results => {
+                secondInput.value = results.rates[0]["rate_for_amount"];
+            })
+            .then(data => console.log(data));
+    }
+});
+
+secondInput.addEventListener('input', function(event) {
+
+    let firstCurrency = firstCurrencySelector.value;
+    let secondCurrency = secondCurrencySelector.value;
+
+    let secondValue = secondInput.value;
+
+    if(secondValue === ""){
+        firstInput.value = "";
+    } else {
+        fetch("https://api.getgeoapi.com/v2/currency/convert" +
+            "?api_key=f47ba4dffba7bea0f2c859a1971e10c3f0bc4255" +
+            "&from=" + secondCurrency +
+            "&to=" + firstCurrency +
+            "&amount=" + secondValue +
+            "&format=json")
+            .then(response => response.json())
+            .then(results => {
+                firstInput.value = results.rates[0]["rate_for_amount"];
+            })
+            .then(data => console.log(data));
+    }
+});
